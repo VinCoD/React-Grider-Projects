6f71d04617fded1d813b6604ddc70f39c22e8673
VinCoD <5895.2020@students.ku.ac.ke>
2023-08-20 10:45:11 +0300

    COmpleted seasons project

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
+
 // import App from './App';
+
 
 class App extends React.Component{
 
-    constructor(props) {
-        super(props);
 
-        // This is the only time we do direct assignment - During Initialization
-        // we have to use this.state to do the initialization
-        this.state = {lat: null, errorMessage: ""};
+    state = {lat: null, errorMessage: ""};
 
+
+    componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
             (position) => {
                 // we called setState function to update our state
@@ -21,14 +23,23 @@ class App extends React.Component{
             }
         );
     }
-    render(){
+
+    renderContent(){
         if(this.state.errorMessage && !this.state.lat){
             return <div>Error: {this.state.errorMessage}</div>
         }
         if(!this.state.errorMessage && this.state.lat){
-            return <div>Lat: {this.state.lat}</div>
+            return <SeasonDisplay lat={this.state.lat}/>
         }
-        return <div>Loading...</div>
+        return <Spinner message="Please accept location request"/>
+    }
+
+    render(){
+        return(
+            <div className="border red">
+                {this.renderContent()}
+            </div>
+        )
 
     }
 }
