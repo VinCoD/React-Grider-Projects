8928a09d68501cd87a96f65d7b5edb110b5dfd3d
VinCoD <5895.2020@students.ku.ac.ke>
2023-09-01 12:37:26 +0300

    Started pics project

---
+++ pics/src/components/App.js
@@ -0,0 +1,10 @@
+import SearchBar from "./SearchBar";
+function App() {
+  return (
+    <div className="ui container" style={{marginTop: "10px"}}>
+      <SearchBar/>
+    </div>
+  );
+}
+
+export default App;
